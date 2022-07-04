a533628fdd58f3566a01841b9f8bca1607ccf26d
JohnathanIssac <98893328+JohnathanIssac@users.noreply.github.com>
2022-07-04 21:23:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,90 @@
 function done(){
     document.getElementById("done").src="Pic/download.png"
 }
+function done1(){
+    document.getElementById("done1").src="Pic/download.png"
+}
+function done2(){
+    document.getElementById("done2").src="Pic/download.png"
+}
+function done3(){
+    document.getElementById("done3").src="Pic/download.png"
+}
+function done4(){
+    document.getElementById("done4").src="Pic/download.png"
+}
+function done5(){
+    document.getElementById("done5").src="Pic/download.png"
+}
+function done6(){
+    document.getElementById("done6").src="Pic/download.png"
+}
+function done7(){
+    document.getElementById("done7").src="Pic/download.png"
+}
+function done8(){
+    document.getElementById("done8").src="Pic/download.png"
+}
+function done9(){
+    document.getElementById("done9").src="Pic/download.png"
+}
+function done10(){
+    document.getElementById("done10").src="Pic/download.png"
+}
+function done11(){
+    document.getElementById("done11").src="Pic/download.png"
+}
+function done12(){
+    document.getElementById("done12").src="Pic/download.png"
+}
+function done13(){
+    document.getElementById("done13").src="Pic/download.png"
+}
+function done14(){
+    document.getElementById("done14").src="Pic/download.png"
+}
+function done15(){
+    document.getElementById("done15").src="Pic/download.png"
+}
+function done16(){
+    document.getElementById("done16").src="Pic/download.png"
+}
+function done17(){
+    document.getElementById("done17").src="Pic/download.png"
+}
+function done18(){
+    document.getElementById("done18").src="Pic/download.png"
+}
+function done19(){
+    document.getElementById("done19").src="Pic/download.png"
+}
+function done20(){
+    document.getElementById("done20").src="Pic/download.png"
+}
+function done21(){
+    document.getElementById("done21").src="Pic/download.png"
+}
+function done22(){
+    document.getElementById("done22").src="Pic/download.png"
+}
+function done23(){
+    document.getElementById("done23").src="Pic/download.png"
+}
+function done24(){
+    document.getElementById("done24").src="Pic/download.png"
+}
+function done25(){
+    document.getElementById("done25").src="Pic/download.png"
+}
+function done26(){
+    document.getElementById("done26").src="Pic/download.png"
+}
+function done27(){
+    document.getElementById("done27").src="Pic/download.png"
+}
+function done28(){
+    document.getElementById("done28").src="Pic/download.png"
+}
+function done29(){
+    document.getElementById("done29").src="Pic/download.png"
+}
